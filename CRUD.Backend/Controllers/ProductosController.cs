41811c5e843e954b6e16887a4239266c33be0487
El Bronco <2020-0288@uteco.edu.do>
2025-08-22 08:19:35 -0400

    Controladores web API para Producto

---
+++ CRUD.Backend/Controllers/ProductosController.cs
@@ -0,0 +1,113 @@
+﻿using CRUD.Backend.Data;
+using CRUD.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductosController : ControllerBase
+{
+    private readonly DataContext _context;
+    // Constructor that accepts a DataContext instance
+    public ProductosController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        var productos = await _context.Productos.ToListAsync();
+        return Ok(productos);
+    }
+
+    
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Producto producto)
+    {
+
+        try
+        {
+            if (producto == null)
+            {
+                return BadRequest("Producto cannot be null");
+            }
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAsync), new { id = producto.Id }, producto);
+        }
+        catch (DbUpdateException ex)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.InnerException!.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+        return Ok(producto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, Producto producto)
+    {
+        if (id != producto.Id)
+        {
+            return BadRequest("ID mismatch");
+        }
+
+        _context.Entry(producto).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return Ok(producto);
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.InnerException!.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+
+        _context.Productos.Remove(producto);
+        try
+        {
+            await _context.SaveChangesAsync();
+            return Ok(producto);
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.InnerException!.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}

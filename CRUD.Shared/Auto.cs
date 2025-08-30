e514cef3b7850294d1eb12fc96818e50a60e59ce
El Bronco <2020-0288@uteco.edu.do>
2025-08-29 22:56:52 -0400

    Clase Auto

---
+++ CRUD.Shared/Auto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Shared;
+
+public class Auto
+{
+    [Key]
+    public int Id { get; set; }
+    public string Marca { get; set; } = string.Empty;
+    public string Modelo { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public decimal Precio { get; set; }
+}
